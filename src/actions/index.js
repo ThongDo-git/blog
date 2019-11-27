5dd9807ec7a1d81bcf729b7fcc0b2622addc323b
Thong Do <tdo05824@gmail.com>
2019-11-27 23:08:15 +0700

    there is another error

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,11 +6,11 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUsers = () => async dispatch => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/user/${id}`);
 
   dispatch({
-    type: "FETCH_USERS",
-    payload: response
+    type: "FETCH_USER",
+    payload: response.data
   });
 };
