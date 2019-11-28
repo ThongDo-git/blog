780e899b34dfc7082dfc2f27ef81169688a5c630
Thong Do <tdo05824@gmail.com>
2019-11-28 09:06:58 +0700

    wait to fix

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -8,7 +8,12 @@ class UserHeader extends Component {
   }
 
   render() {
-    return <div>User Header</div>;
+    const user = this.props.users.find(user => user.id === this.props.userId);
+    if (!user) {
+      return null;
+    }
+
+    return <div className="header">{user.name}</div>;
   }
 }
 
@@ -16,4 +21,4 @@ const mapStateToProps = state => {
   return { users: state.users };
 };
 
-export default connect(null, { fetchUser })(UserHeader);
+export default connect(mapStateToProps, { fetchUser })(UserHeader);
